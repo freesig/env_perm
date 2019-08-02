37bf63e06c41893118a5dfbf4b38e6887a2778f7
Tom Gowan <s3549186@student.rmit.edu.au>
2019-08-02 11:18:59 +1000

    make example generic

--- examples/test_profiles.rs
+++ examples/test_profiles.rs
@@ -1,24 +1,23 @@
 use std::fs::OpenOptions;
-use std::path::Path;
 
 fn main() {
-    let mut profile = Path::new("/Users/tomgowan").to_path_buf();
+    let mut profile = dirs::home_dir().unwrap();
     profile.push(".bash_profile");
     let mut oo = OpenOptions::new();
-    oo.append(true)
-        .create(false);
-    let r = oo.open(profile.clone())
-        .or_else(|_|{
+    oo.append(true).create(false);
+    let r = oo
+        .open(profile.clone())
+        .or_else(|_| {
             profile.pop();
             profile.push(".bash_login");
             oo.open(profile.clone())
         })
-        .or_else(|_|{
+        .or_else(|_| {
             profile.pop();
             profile.push(".profile");
             oo.open(profile.clone())
         })
-        .or_else(|_|{
+        .or_else(|_| {
             profile.pop();
             profile.push(".bash_profile");
             oo.create(true);
